aa4a287e363a80b90fd7b6d4df58746f68541482
jh <jh2two@gmail.com>
2023-12-03 23:47:57 +0900

    Feat:    add Question and Answer views

--- repybo/admin.py
+++ repybo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["subject"]
+
+admin.site.register(models.Question, QuestionAdmin)
+admin.site.register(models.Answer)
--- repybo/urls.py
+++ repybo/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("<int:question_id>/", views.detail),
+
+    ### generic view 연습
+    # path("", views.IndexView.as_view()),
+    # path("<int:pk>/", views.DetailView.as_view()),
 ]
