caf5cbe9f8911f482503f6e8bdb717d6f9d084d4
jh <jh2two@gmail.com>
2023-12-06 22:04:24 +0900

    Feat:add a paginator

--- repybo/views.py
+++ repybo/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
+    page = request.GET.get("page", "1")
     question_list = Question.objects.order_by("-created_date")
 
-    context = {"question_list": question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+
+    context = {"question_list": page_obj}
     return render(request, "repybo/question_list.html", context)
 
 
