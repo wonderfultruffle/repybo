6107744774f83f4dd0f13dbfa871f4c9a45e5f03
occkjh <toomanyjeong@googl.com>
2023-12-06 14:05:06 +0900

    Feat: complete login work

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("pybo/", include("repybo.urls")),
+    path("common/", include("common.urls")),
 ]
