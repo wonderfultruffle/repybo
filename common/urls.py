7656dc6790a15601fd309e64a2b7135b396fb9ff
occkjh <toomanyjeong@googl.com>
2023-12-06 16:34:33 +0900

    Chore: add missed file

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from . import views
+
 app_name = "common"
 
 urlpatterns = [
-    path("question/create/", auth_views.LoginView.as_view(template_name="common/login.html"), name="login"),
-    path("question/create/", auth_views.LogoutView.as_view(), name="logout"),
+    path("login/", auth_views.LoginView.as_view(template_name="common/login.html"), name="login"),
+    path("logput/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.signup, name="signup")
 ]
