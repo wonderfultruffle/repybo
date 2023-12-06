a6b7417a0854a45dba7562a4971002bbb20b2ade
occkjh <toomanyjeong@googl.com>
2023-12-06 14:09:55 +0900

    Feat: set url for login

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from repybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("pybo/", include("repybo.urls")),
     path("common/", include("common.urls")),
+    path("", views.index, name="index"),
 ]
