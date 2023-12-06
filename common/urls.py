fe6b2e6a3213ff989dcb393fef7945d023f72281
jh <jh2two@gmail.com>
2023-12-07 00:30:00 +0900

    Feat: add au author field to Question and Answer

--- common/urls.py
+++ common/urls.py
@@ -7,6 +7,6 @@ app_name = "common"
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="common/login.html"), name="login"),
-    path("logput/", auth_views.LogoutView.as_view(), name="logout"),
-    path("signup/", views.signup, name="signup")
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.signup, name="signup"),
 ]
--- repybo/views.py
+++ repybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseNotAllowed
+# from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 from .models import Question
 from .forms import QuestionForm, AnswerForm
@@ -36,27 +37,33 @@ def detail(request, question_id):
     return render(request, "repybo/question_detail.html", context)
 
 
+@login_required(login_url="common:login")
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    # question.answer_set.create(content=request.POST.get("content"))
-    #
-    # return redirect("pybo:detail", question_id=question.id)
+
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
             answer.question = question
+            answer.author = request.user
             answer.save()
             return redirect("pybo:detail", question_id=question.id)
     else:
-        return HttpResponseNotAllowed("Only POST is allowed.")
+        form = AnswerForm()
+
+    context = {"question": question, "form": form}
+    return render(request, "repybo/question_detail.html", context)
 
 
+@login_required(login_url="common:login")
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
-            form.save()
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
             return redirect("pybo:index")
     else:
         form = QuestionForm()
